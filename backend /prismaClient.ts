9df6417f4754f3e3956cf9e4146a3c551e1b6327
Bhanu🧑‍💻 <bhanu934785@gmail.com>
2025-12-12 19:18:26 +0530

    Update prismaClient.ts

--- backend /prismaClient.ts
+++ backend /prismaClient.ts
@@ -10,13 +10,3 @@ const prisma = global.prisma || new PrismaClient();
 if (process.env.NODE_ENV === 'development') global.prisma = prisma;
 
 export default prisma;
-
-
-
-
-import prisma from './prismaClient';
-
-async function example() {
-  const users = await prisma.user.findMany();
-  console.log(users);
-}
